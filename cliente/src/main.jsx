d0dd5023c55e73d1e24fc66834b4913acc0f37df
Phenrique1628 <henriquepedro02964@gmail.com>
2024-12-16 17:34:10 -0400

    envio1

---
+++ cliente/src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Registro from './pages/Registro'
+import Alterar from './pages/Alterar'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+       <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/users" element={<Registro/>}/>
+            <Route path="/alterar/:id" element={<Alterar/>}/>
+       </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
